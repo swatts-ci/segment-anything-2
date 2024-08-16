41198e43cdb3c56eef6278211b06b1a6e59febe7
swatts-ci <sam.watts@continuum.industries>
2024-08-16 10:00:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 # Required dependencies
 REQUIRED_PACKAGES = [
     "torch>=2.2.2",
-    "torchvision>=0.18.1",
+    "torchvision>=0.17",
     "numpy>=1.24.4",
     "tqdm>=4.66.1",
     "hydra-core>=1.3.2",
